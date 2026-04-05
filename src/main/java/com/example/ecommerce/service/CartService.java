e4e7bfb284fd030d821d87214a7f28fffb370473
Pooja <poojachincholkar33@gmail.com>
2026-04-05 19:09:29 +0530

    Updated

--- src/main/java/com/example/ecommerce/service/CartService.java
+++ src/main/java/com/example/ecommerce/service/CartService.java
@@ -37,8 +37,12 @@ public class CartService
 	@Transactional
 	public Cart getCart(String username)
 	{
+		User user=userRepository.findByUsername(username)
+				.orElseThrow(()-> new UserNotFoundException("User not found"));
+	Cart cart= cartRepository.findByUser(user)
+				.orElseThrow(() -> new CartNotFoundException("Cart not found for user"));
 	
-	return getOrCreateCart(username);
+	return cart;
 	}
 	
 	@Transactional
